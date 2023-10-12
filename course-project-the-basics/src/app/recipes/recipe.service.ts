c13a9dfadd2415d4095c2a52c2c1e52e66bed1d7
Topaz712 <tzam712@gmail.com>
2023-10-11 19:53:35 -0500

    used a service for cross-component communication

--- course-project-the-basics/src/app/recipes/recipe.service.ts
+++ course-project-the-basics/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model'
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/4551832/pexels-photo-4551832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
     new Recipe('Another Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/4551832/pexels-photo-4551832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
