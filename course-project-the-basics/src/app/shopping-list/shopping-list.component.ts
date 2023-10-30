1f5da3476c3b8b798d2012d75a4e5b405542fdb5
Topaz712 <tzam712@gmail.com>
2023-10-29 21:55:55 -0500

    Add:(shopping-list/edit) setup selection item edit

--- course-project-the-basics/src/app/shopping-list/shopping-list.component.ts
+++ course-project-the-basics/src/app/shopping-list/shopping-list.component.ts
@@ -11,18 +11,22 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy{
   ingredients: Ingredient[];
-  private igChangeSub: Subscription;
+  private subscription : Subscription;
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this. slService.getIngredients();
-    this.igChangeSub = this.slService.ingredientsChanged
+    this.subscription = this.slService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
       );
+  }
+
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
   }
 
   ngOnDestroy(): void {
