4298ef4e7ff574b2585bc25e71ed71cc75a83ac4
Topaz712 <tzam712@gmail.com>
2023-11-08 23:59:01 -0600

    Transformed Response Data

--- course-project-the-basics/src/app/recipes/recipe.service.ts
+++ course-project-the-basics/src/app/recipes/recipe.service.ts
@@ -9,32 +9,33 @@ import { Subject } from 'rxjs/internal/Subject';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-    'Carne Asada Tacos',
-    'Delicious juicy and tender beef tacos!',
-    'https://images.pexels.com/photos/8999152/pexels-photo-8999152.jpeg?auto=compress&cs=tinysrgb&w=1600',
-    [
-      new Ingredient('Meat', 1),
-      new Ingredient('Corn Tortillas', 2),
-      new Ingredient('Salsa', 2),
-      new Ingredient('Onion', 1),
-      new Ingredient('Cilantro', 1),
-      new Ingredient('Lime', 1)
-    ]),
-    new Recipe(
-      'Ramen Bowl',
-      'Yummy Ramen to satiate your hunger!',
-      'https://images.pexels.com/photos/15085069/pexels-photo-15085069/free-photo-of-food-restaurant-dinner-lunch.jpeg?auto=compress&cs=tinysrgb&w=1600',
-      [
-        new Ingredient('Noodles', 1),
-        new Ingredient('Eggs', 1),
-        new Ingredient('Chicken', 1),
-        new Ingredient('Broth', 1),
-        new Ingredient('Green Onion', 1),
-        new Ingredient('Seaweed', 1)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //   'Carne Asada Tacos',
+  //   'Delicious juicy and tender beef tacos!',
+  //   'https://images.pexels.com/photos/8999152/pexels-photo-8999152.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  //   [
+  //     new Ingredient('Meat', 1),
+  //     new Ingredient('Corn Tortillas', 2),
+  //     new Ingredient('Salsa', 2),
+  //     new Ingredient('Onion', 1),
+  //     new Ingredient('Cilantro', 1),
+  //     new Ingredient('Lime', 1)
+  //   ]),
+  //   new Recipe(
+  //     'Ramen Bowl',
+  //     'Yummy Ramen to satiate your hunger!',
+  //     'https://images.pexels.com/photos/15085069/pexels-photo-15085069/free-photo-of-food-restaurant-dinner-lunch.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  //     [
+  //       new Ingredient('Noodles', 1),
+  //       new Ingredient('Eggs', 1),
+  //       new Ingredient('Chicken', 1),
+  //       new Ingredient('Broth', 1),
+  //       new Ingredient('Green Onion', 1),
+  //       new Ingredient('Seaweed', 1)
+  //     ])
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {
 
--- course-project-the-basics/src/app/shared/data-storage.service.ts
+++ course-project-the-basics/src/app/shared/data-storage.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -28,6 +29,11 @@ export class DataStorageService {
   fetchRecipes() {
     this.http
     .get<Recipe[]>('https://ng-course-recipe-book-f8ed6-default-rtdb.firebaseio.com/recipes.json')
+    .pipe(map(recipes => {
+      return recipes.map(recipe => {
+        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+      });
+    }))
     .subscribe(recipes => {
       this.recipeService.setRecipes(recipes);
     });
