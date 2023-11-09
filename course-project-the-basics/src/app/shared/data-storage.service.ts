33c8631d79c41f310c530f4d45421d44d4f4d605
Topaz712 <tzam712@gmail.com>
2023-11-08 23:41:58 -0600

    Added: Fetching Data/Recipes

--- course-project-the-basics/src/app/shared/data-storage.service.ts
+++ course-project-the-basics/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,13 @@ export class DataStorageService {
         .subscribe(response =>  {
           console.log(response);
         });
+  }
+
+  fetchRecipes() {
+    this.http
+    .get<Recipe[]>('https://ng-course-recipe-book-f8ed6-default-rtdb.firebaseio.com/recipes.json')
+    .subscribe(recipes => {
+      this.recipeService.setRecipes(recipes);
+    });
   }
 }
